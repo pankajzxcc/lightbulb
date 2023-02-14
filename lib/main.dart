e2fb910530ba497ca25ed89a00174313266749a9
pankaj kumar <pankajzxcc@gmail.com>
2023-02-14 14:57:11 +0530

    second commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,70 @@
-import'package:flutter/material.dart';
-void main(){
-  runApp( const MaterialApp(
-    debugShowCheckedModeBanner: false,
-    home: HomePage(),
-  ),);
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+
+void main() {
+  runApp(
+    const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    ),
+  );
 }
+
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
   @override
-
   State<HomePage> createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
+  bool _switchValue = false;
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Image(
-          image: AssetImage('images/light bulb off.jpg',),
-          fit: BoxFit.cover,
-          height: double.infinity,
-          width: double.infinity,
-        ),
+    return Scaffold(
+      body: Stack(
+        children: [
+          Container(
+              child: _switchValue == false
+                  ? const Image(
+                      image: AssetImage(
+                        'images/light bulb off.jpg',
+                      ),
+                      fit: BoxFit.cover,
+                      height: double.infinity,
+                      width: double.infinity,
+                    )
+                  : const Image(
+                      image: AssetImage(
+                        'images/light-bulb-on.png',
+                      ),
+                      fit: BoxFit.cover,
+                      height: double.infinity,
+                      width: double.infinity,
+                    ),
+          ),
+
+          Positioned(
+            bottom: 60,
+            right: 160,
+            child: CupertinoSwitch(
+              value: _switchValue,
+              onChanged: (value) {
+                setState(() {
+                  _switchValue = value;
+                }
+                );
+              },
+              thumbColor: const Color(0xff76ff03),
+              activeColor: const Color(0xff76ff03),
+              trackColor: const Color(0xffffffff),
+
+
+            ),
+
+          ),
+
+        ],
       ),
     );
   }
